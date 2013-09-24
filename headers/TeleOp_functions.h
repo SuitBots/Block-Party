8855391fa57c573bc9104c053fe69771eadf4366
Fletcher Porter <fp.monkey@gmail.com>
2013-09-24 16:59:57 -0700

    added temporary rear drive motors

--- headers/TeleOp_functions.h
+++ headers/TeleOp_functions.h
@@ -14,6 +14,8 @@
 // tMotor DriveMotors[] = { DriveFL, DriveFR, DriveBL, DriveBR };
 //
 
+#include "JoystickDriver.c"
+
 int J1X1()  { return joystick.joy1_x1; }       // These lines make pretty variables for the joysticks
 int J1X2()  { return joystick.joy1_x2; }       // J is which logitech controller is being used
 int J1Y1()  { return joystick.joy1_y1; }       // X or Y is the axis on which the compiler will read values
@@ -23,54 +25,52 @@ int J2X2()  { return joystick.joy2_x2; }
 int J2Y1()  { return joystick.joy2_y1; }
 int J2Y2()  { return joystick.joy2_y2; }
 
-void tank_drive(tMotor *DriveMotors, size_t numMotors) {
-  if (numMotors != 2) {
-    writeDebugStream('Error: there must be exactly two motors for drive function');
-    return;
-  }
-  DriveMotors[0] = J1Y1();
-  DriveMotors[1] = J1Y2();
+void tank_drive(tMotor *DriveMotors) {
+  motor[DriveMotors[0]] = J1Y1();
+  motor[DriveMotors[1]] = J1Y2();
 }
 
-void arcade_drive(tMotor *DriveMotors, size_t numMotors) {
-  if (numMotors != 2) {
-    writeDebugStream('Error: there must be exactly two motors for drive function');
-    return;
-  }
-  DriveMotors[0] = J1Y2() - J1X2();
-  DriveMotors[1] = J1Y2() + J1X2();
+void arcade_drive(tMotor *DriveMotors, int size) {
+	if (size == 2) {
+  	motor[DriveMotors[0]] = J1Y2() - J1X2();
+  	motor[DriveMotors[1]] = J1Y2() + J1X2();
+	}
+	if (size == 4) {
+		motor[DriveMotors[0]] = J1Y2() - J1X2();
+  	motor[DriveMotors[1]] = J1Y2() + J1X2();
+  	motor[DriveMotors[3]] = J1Y2() - J1X2();
+  	motor[DriveMotors[4]] = J1Y2() + J1X2();
+	}
 }
+
+
 
 // for an omniwheel drive train
-void omni_drive(tMotor *DriveMotors, size_t numMotors) {
-  if (numMotors != 4) {
-    writeDebugStream('Error: there must be exactly four motors for drive function');
-    return;
-  }
-  DriveMotors[0] = J1Y2() - J1X2() + J1X1();
-  DriveMotors[1] = J1Y2() + J1X2() - J1X1();
-  DriveMotors[2] = J1Y2() - J1X2() - J1X1();
-  DriveMotors[3] = J1Y2() + J1X2() + J1X1();
-}
+//void omni_drive(tMotor *DriveMotors) {
+//  DriveMotors[0] = J1Y2() - J1X2() + J1X1();
+//  DriveMotors[1] = J1Y2() + J1X2() - J1X1();
+//  DriveMotors[2] = J1Y2() - J1X2() - J1X1();
+//  DriveMotors[3] = J1Y2() + J1X2() + J1X1();
+//}
 
-int compassBearing() { return 0; } // TODO: figure out a rotation sensor
+//int compassBearing() { return 0; } // TODO: figure out a rotation sensor
 
-void omnimove_in_direction(int angle) {
-  float xval = MAX_JOY_VAL * cos(angle);
-  float yval = MAX_JOY_VAL * sin(angle);
+//void omnimove_in_direction(int angle) {
+//  float xval = MAX_JOY_VAL * cos(angle);
+//  float yval = MAX_JOY_VAL * sin(angle);
 
-  motor[DriveFR] = yval - xval + J1X1();
-  motor[DriveFL] = yval + xval - J1X1();
-  motor{DriveBL] = yval - xval - J1X1();
-  motor{DriveBR] = yval + xval + J1X1();
-}
+//  motor[DriveFR] = yval - xval + J1X1();
+//  motor[DriveFL] = yval + xval - J1X1();
+//  motor{DriveBL] = yval - xval - J1X1();
+//  motor{DriveBR] = yval + xval + J1X1();
+//}
 
-void smart_omni_drive(tMotor *DriveMotors, size_t numMotors) {
-  // smart means you can passivly rotate and go forward at the same time
-  if (numMotors != 4) {
-    writeDebugStream('Error: there must be exactly four motors for drive function');
-    return;
-  }
-  int joystickAngle = atan(J1Y2() / J1X2());
-  move_in_direction(joystickAngle - compassBearing());
-}
+//void smart_omni_drive(tMotor *DriveMotors, size_t numMotors) {
+//  // smart means you can passivly rotate and go forward at the same time
+//  if (numMotors != 4) {
+//    writeDebugStream('Error: there must be exactly four motors for drive function');
+//    return;
+//  }
+//  int joystickAngle = atan(J1Y2() / J1X2());
+//  move_in_direction(joystickAngle - compassBearing());
+//}
