87ed1ed97af0ab435eafab4de86e3d9ae3d0b33b
Fletcher Porter <fp.monkey@gmail.com>
2013-09-24 16:22:00 -0700

    Created HiTechnic_Gyro_Sensor_Test.c
    Prints reading offeset and gyro output on screen. Uses sensor port 1.

---
+++ source/HiTechnic_Gyro_Sensor_Test.c
@@ -0,0 +1,34 @@
+#pragma config(Sensor, S1,     GYRO,              sensorAnalogInactive)
+
+#include "drivers/hitechnic-gyro.h"
+
+
+task main () {
+
+  eraseDisplay();
+  while(true) {
+      eraseDisplay();
+      nxtDisplayTextLine(1, "Resetting");
+      nxtDisplayTextLine(2, "offset");
+      wait1Msec(500);
+
+      // Start the calibration and display the offset
+      nxtDisplayTextLine(2, "Offset: %f", HTGYROstartCal(GYRO));
+
+
+    while(nNxtButtonPressed != kEnterButton) {
+      eraseDisplay();
+
+      nxtDisplayTextLine(1, "Reading");
+      // Read the current calibration offset and display it
+      nxtDisplayTextLine(2, "Offset: %4f", HTGYROreadCal(GYRO));
+
+      nxtDisplayClearTextLine(4);
+      // Read the current rotational speed and display it
+      nxtDisplayTextLine(4, "Gyro:   %4f", HTGYROreadRot(GYRO));
+      nxtDisplayTextLine(6, "Press enter");
+      nxtDisplayTextLine(7, "to reset zero");
+      wait1Msec(100);
+    }
+  }
+}
